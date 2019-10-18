99b8a33da637e782982d4e3627139d5e0f3ec381
fiaismi25 <56153784+fiaismi25@users.noreply.github.com>
2019-10-18 15:54:48 +0700

    tugas

---
+++ main.py
@@ -0,0 +1,14 @@
+nama = 'fiaisminura'
+program = 'usaha'
+
+print(f'program {program} oleh {nama}')
+
+def hitung_usaha(gaya, jarak):
+    usaha = gaya * jarak
+    print(f'gaya = {gaya * 20}newton menempuh jarak = 10')
+    print(f'sehingga usaha = {usaha} joule')
+
+#gaya = 20
+#jarak = 10
+usaha = hitung_usaha(20, 10)
+usaha = hitung_usaha(15,150)
